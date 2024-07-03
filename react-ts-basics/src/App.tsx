45c832a0c5122ea65538a26713d8af59605c8c11
VictorBotev12 <160576550+VictorBotev12@users.noreply.github.com>
2024-07-03 16:10:48 +0300

    main

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,8 +1,15 @@
 import CourseGoal from "./components/CourseGoal";
+import goalsImg from "./assets/goals.jpg";
+import Header from "./components/Header";
 export default function App() {
   return (
     <main>
-      <CourseGoal title="Learn React + TS">Learn it from here</CourseGoal>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your course goals</h1>
+      </Header>
+      <CourseGoal title="Learn React + TS">
+        Learn it from the ground up
+      </CourseGoal>
     </main>
   );
 }
